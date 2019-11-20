383f7d2fa7f5e87bdfd8ddab892c547bd7556e9b
tjadhav <tusharjadhav219@gmail.com>
2019-11-20 20:42:22 +0530

    Showing images in the Grid layout

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,18 +1,13 @@
 import React from "react";
+import "./ImageList.css";
+import ImageCard from "./ImageCard";
 
 const ImageList = props => {
-  return props.images.map(({ id, description, urls }) => {
-    return (
-      <div key={id}>
-        <img
-          key={id}
-          src={urls.regular}
-          alt={description}
-          title={description}
-        />
-      </div>
-    );
+  const images = props.images.map(image => {
+    return <ImageCard key={image.id} image={image} />;
   });
+
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
