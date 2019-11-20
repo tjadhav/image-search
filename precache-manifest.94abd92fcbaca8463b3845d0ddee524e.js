5e91ae6f6af79961403cd3c6ba8c8a72ec4ffc6b
tjadhav <tjadhav@tibco.com>
2019-11-20 14:43:08 +0530

    Updates

---
+++ precache-manifest.94abd92fcbaca8463b3845d0ddee524e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "154e602db0cf15b52b2ee390d80b7d2d",
+    "url": "/image-search/index.html"
+  },
+  {
+    "revision": "6b1e9fc955e7ea214b1f",
+    "url": "/image-search/static/js/2.ecb0ebfd.chunk.js"
+  },
+  {
+    "revision": "778588eb9c073d01dda5",
+    "url": "/image-search/static/js/main.600b5d63.chunk.js"
+  },
+  {
+    "revision": "9f484bc4be5bb529a9c6",
+    "url": "/image-search/static/js/runtime-main.b3fe5ed4.js"
+  }
+]);
